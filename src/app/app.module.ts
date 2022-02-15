4fab67de34d0e28a72ade67b4144401067346a61
frogKim93 <84324343+frogkim93@users.noreply.github.com>
2022-02-15 21:24:25 +0900

    feat: 로그인 화면 작성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { AppComponent } from './component/app/app.component';
+import { LoginComponent } from './component/login/login.component';
+import { RouteConfig } from './config/router.config';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouteConfig
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "login",
+    templateUrl: "./view/login.component.html",
+    providers: []
+})
+export class LoginComponent {
+    
+}
