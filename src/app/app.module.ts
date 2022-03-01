f9b6be5308ac78a29c77ad89c7131ab3b70879cf
frogKim93 <84324343+frogkim93@users.noreply.github.com>
2022-03-01 22:14:23 +0900

    feat: 팀짜기 화면 구성 완료

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { AppComponent } from './component/app/app.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { LoginComponent } from './component/login/login.component';
 import { MainComponent } from './component/main/main.component';
+import { MakeTeamComponent } from './component/make-team/make-team.component';
 import { AddMemberComponent } from './component/memberList/add-member/add-member.component';
 import { MemberListComponent } from './component/memberList/memberList.component';
 import { MenuComponent } from './component/menu/menu.component';
+import { SummaryComponent } from './component/summary/summary.component';
 import { RouteConfig } from './config/router.config';
 
 @NgModule({
@@ -17,6 +19,8 @@ import { RouteConfig } from './config/router.config';
     MenuComponent,
     MemberListComponent,
     AddMemberComponent,
+    MakeTeamComponent,
+    SummaryComponent,
     FooterComponent
   ],
   imports: [
---
+++ src/app/component/summary/summary.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "summary",
+    templateUrl: "./view/summary.component.html"
+})
+export class SummaryComponent {
+    
+}
