288e77a9a36c3486c3ec1d472b92ccbec7b79d42
hyungtack <84324343+Polo-Kim@users.noreply.github.com>
2022-03-15 22:53:59 +0900

    feat: 퍼블리싱 밑작업 완료

--- src/app/service/menu.service.ts
+++ src/app/service/menu.service.ts
@@ -3,7 +3,7 @@ import { MenuCode } from "../const/menu.code";
 
 @Injectable()
 export class MenuService {
-    private currentMenu: MenuCode = MenuCode.CONFIG;
+    private currentMenu: MenuCode = MenuCode.MAIN;
 
     public getCurrentMenu(): MenuCode {
         return this.currentMenu;
